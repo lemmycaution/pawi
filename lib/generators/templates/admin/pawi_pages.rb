d589e997829e53bb47eb6b9391f7f5712f7da08d
Onur Uyar <fluxproject@gmail.com>
2012-01-23 14:14:30 +0200

    admin moved templates

---
+++ lib/generators/templates/admin/pawi_pages.rb
@@ -0,0 +1,30 @@
+ActiveAdmin.register Pawi::Page do
+  
+  menu :parent => "Content"
+  
+  controller do
+    cache_sweeper Pawi::Sweeper
+  end
+  
+  index do
+    column :id
+    column :format    
+    column :name, :sortable => :name
+    column :created_at, :sortable => :created_at
+    column :updated_at, :sortable => :updated_at
+    default_actions
+  end
+  
+  form do |f|
+    f.inputs do
+      f.input :name
+      f.input :format      
+      f.input :body
+    end
+    f.inputs do    
+      f.input :widgets, :as => :check_boxes, :collection => Pawi::Widget.all
+    end
+    
+    f.buttons
+  end
+end
---
+++ lib/generators/templates/admin/pawi_widgets.rb
@@ -0,0 +1,29 @@
+ActiveAdmin.register Pawi::Widget do
+  
+  menu :parent => "Content"
+  
+  controller do
+    cache_sweeper Pawi::Sweeper
+  end
+  
+  index do
+    column :id
+    column :name, :sortable => :name
+    column :created_at, :sortable => :name
+    column :updated_at, :sortable => :name
+    default_actions
+  end
+  
+  form do |f|
+    f.inputs do
+      f.input :name
+      f.input :body      
+      f.input :position       
+    end
+    f.inputs do    
+      f.input :pages, :as => :check_boxes, :collection => Pawi::Page.all
+    end
+    
+    f.buttons
+  end  
+end
